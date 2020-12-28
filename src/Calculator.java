6ce1b97b0903e9e52a08d9da9476cb3863fa4eb5
bong6981 <kbk5190@naver.com>
2020-12-28 20:22:06 +0900

    입력값 여러 개 받아서 계싼하기

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,34 +1,43 @@
-import java.util.Scanner;
+import java.util.Scanner; 
 
-public class Calculator {
+public class Calculator{
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
-		String input = sc.nextLine();
-		String[] input_array = input.split("");
-		if ("+".equals(input_array[1])) {
-			double x = Double.parseDouble(input_array[0]);
-			double y = Double.parseDouble(input_array[2]);
-			double result = x + y;
-			System.out.println(input_array[0] + "+" + input_array[2] + "=" + result);
-		// 파이썬에서는 if elif else를 썼는데 java는 if, else if, else를 사용한다 
-		} else if ("-".equals(input_array[1])) {
-			double x = Double.parseDouble(input_array[0]);
-			double y = Double.parseDouble(input_array[2]);
-			double result = x - y;
-			System.out.println(input_array[0] + "-" + input_array[2] + "=" + result);
-		} else if ("*".equals(input_array[1])) {
-			double x = Double.parseDouble(input_array[0]);
-			double y = Double.parseDouble(input_array[2]);
-			double result = x * y;
-			System.out.println(input_array[0] + "*" + input_array[2] + "=" + result);
-		} else {
-			double x = Double.parseDouble(input_array[0]);
-			double y = Double.parseDouble(input_array[2]);
-			double result = x / y;
-			System.out.println(input_array[0] + "/" + input_array[2] + "=" + result);
-		} 
-				
+		System.out.println("첫번째 값을 입력하세요 : ");
+		int first = sc.nextInt(); 
+		System.out.println(first);
+		
+		
+		//파이썬에서는 True, False를 대문자로 썼는데, java에서는 true로 쓴다 
+		while(true) {
+			System.out.println("사칙연산 값을 입력하세요 : ");
+			String buho = sc.next();
+			System.out.println(buho);
+			if ("quit".equals(buho)) {
+				break;
+			}
+			System.out.println("다음 값을 입력하세요 : ");
+			int number = sc.nextInt();
+			System.out.println(number);
+			if ("+".equals(buho)) {
+				first = first + number;
+				System.out.println(first);
+			}
+			else if ("-".equals(buho)) {
+				first = first - number;
+				System.out.println(first);
+			}
+			else if ("*".equals(buho)) {
+				first = first * number;
+				System.out.println(first);
+			}
+			else {
+				first = first / number;
+				System.out.println(first);
+			}
+			
+			
+		}
 	}
-
 }
-
