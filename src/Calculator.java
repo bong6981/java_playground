0d33ee43586d93ace6dd80f273f25cedec6cd1d8
bong6981 <kbk5190@naver.com>
2020-12-28 22:26:25 +0900

    메소드를 추가해 로직을 분리

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,42 +1,63 @@
 import java.util.Scanner; 
 
 public class Calculator{
-	public static void main(String[] args) {
+	static Scanner sc = new Scanner(System.in);	
+	public static int input_first() {
 		
-		Scanner sc = new Scanner(System.in);
 		System.out.println("첫번째 값을 입력하세요 : ");
 		int first = sc.nextInt(); 
 		System.out.println(first);
+		return first;		
+	}
+	
+	public static String input_buho( ) {
+		System.out.println("사칙연산 값을 입력하세요 : ");
+		String buho = sc.next();
+		System.out.println(buho);
+		return buho; 
+	}
+	
+	public static int input_number( ) {
+		System.out.println("다음 값을 입력하세요 : ");
+		int number = sc.nextInt();
+		System.out.println(number);		
+		return number;
+	}
+	
+	public static int calculate(int first, String buho, int number) {
+		if ("+".equals(buho)) {
+			int result = first + number;
+			return result;
+		}
+		else if ("-".equals(buho)) {
+			int result = first - number;
+			return result;
+		}
+		else if ("*".equals(buho)) {
+			int result = first * number;
+			return result;
+		}
+		else {
+			int result = first / number;
+			return result;
+		}
 		
-		
-		//파이썬에서는 True, False를 대문자로 썼는데, java에서는 true로 쓴다 
+	}
+	
+	public static void output(int result) {
+		System.out.println("결과값은" + result);
+	}
+	public static void main(String[] args) {
+		int first = input_first();
 		while(true) {
-			System.out.println("사칙연산 값을 입력하세요 : ");
-			String buho = sc.next();
-			System.out.println(buho);
+			String buho = input_buho();
 			if ("quit".equals(buho)) {
 				break;
 			}
-			System.out.println("다음 값을 입력하세요 : ");
-			int number = sc.nextInt();
-			System.out.println(number);
-			if ("+".equals(buho)) {
-				first = first + number;
-				System.out.println(first);
-			}
-			else if ("-".equals(buho)) {
-				first = first - number;
-				System.out.println(first);
-			}
-			else if ("*".equals(buho)) {
-				first = first * number;
-				System.out.println(first);
-			}
-			else {
-				first = first / number;
-				System.out.println(first);
-			}
-			
+			int number = input_number();			
+			int result = calculate(first, buho, number);
+			output(result);
+			first = result;
 			
 		}
 	}
