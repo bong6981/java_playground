e91fe984a9e44a2a401aa0cfae5bdf4f76ae4954
bong6981 <kbk5190@naver.com>
2020-12-29 16:35:25 +0900

    1229 플레이그라운드-구구단-추가요구사항 구현

--- gugudan.py
+++ gugudan.py
@@ -1,2 +1,8 @@
-dan = list(map(int, input("입력하고 싶은 단과 몇 번째까지 구할지?: ").split(","))) 
-print(dan)
+input_user = list(map(int, input("입력하고 싶은 단과 몇 번째까지 구할지?: ").split(","))) 
+dan = input_user[0]
+number = input_user[1]
+for i in range(2, dan+1):
+    array  = []
+    for j in range(1, number+1):
+        array.append(i * j)
+    print(array)
