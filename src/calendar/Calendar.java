6e55a2058aacf66c6963fa7d391b57d79af9801f
bong6981 <kbk5190@naver.com>
2020-12-29 22:35:05 +0900

    캘린더 플레이그라운드 단계 1 구현

---
+++ src/calendar/Calendar.java
@@ -0,0 +1,15 @@
+package calendar;
+import java.util.Scanner; 
+
+public class Calendar {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("두 수를 입력하세요");
+		String input = sc.nextLine();
+		String[] input_array = input.split(" "); 
+		int x = Integer.parseInt(input_array[0]);
+		int y = Integer.parseInt(input_array[1]);
+		int result = x + y;
+		System.out.println("두 수의 합은" + result + "입니다");
+	}
+}
