24c77c7c4d28205f57cb802887b4bb056ae9dee8
bong6981 <kbk5190@naver.com>
2020-12-29 10:50:48 +0900

    문자열로 계산값 받기 구현

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,64 +1,42 @@
 import java.util.Scanner; 
 
 public class Calculator{
-	static Scanner sc = new Scanner(System.in);	
-	public static int input_first() {
-		
-		System.out.println("첫번째 값을 입력하세요 : ");
-		int first = sc.nextInt(); 
-		System.out.println(first);
-		return first;		
-	}
 	
-	public static String input_buho( ) {
-		System.out.println("사칙연산 값을 입력하세요 : ");
-		String buho = sc.next();
-		System.out.println(buho);
-		return buho; 
-	}
+	static int first = 0;
+	static int number = 0;
+	static String buho = " ";
+	static int result = 0; 
 	
-	public static int input_number( ) {
-		System.out.println("다음 값을 입력하세요 : ");
-		int number = sc.nextInt();
-		System.out.println(number);		
-		return number;
-	}
-	
-	public static int calculate(int first, String buho, int number) {
-		if ("+".equals(buho)) {
-			int result = first + number;
-			return result;
-		}
-		else if ("-".equals(buho)) {
-			int result = first - number;
-			return result;
-		}
-		else if ("*".equals(buho)) {
-			int result = first * number;
-			return result;
-		}
-		else {
-			int result = first / number;
-			return result;
-		}
-		
-	}
-	
-	public static void output(int result) {
-		System.out.println("결과값은" + result);
-	}
 	public static void main(String[] args) {
-		int first = input_first();
-		while(true) {
-			String buho = input_buho();
-			if ("quit".equals(buho)) {
-				break;
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		String[] input = str.split(" ");
+	 
+		for (int i = 0; i < input.length; i++) {
+			if (i == 0) {
+				first = Integer.parseInt(input[i]);
+			} else if (i%2 == 1) {
+				buho = input[i];
+			} else {
+				number = Integer.parseInt(input[i]);
+				if("+".equals(buho)) {
+					result = first + number;
+					System.out.println("결과값은" + result);
+				}else if("-".equals(buho)) {
+					result = first - number;
+					System.out.println("결과값은" + result);
+				}else if("*".equals(buho)) {
+					result = first * number;
+					System.out.println("결과값은" + result);
+				}else {
+					result = first / number;
+					System.out.println("결과값은" + result);
+				}
+				first = result; 
 			}
-			int number = input_number();			
-			int result = calculate(first, buho, number);
-			output(result);
-			first = result;
-			
+		
+				
 		}
+		
 	}
 }
