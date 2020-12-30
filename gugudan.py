a789feafa8597cc0ac673683f65ecdc767460d15
bong6981 <kbk5190@naver.com>
2020-12-30 13:09:30 +0900

    playgorund gugudan self-study edit

--- gugudan.py
+++ gugudan.py
@@ -1,8 +1,8 @@
-input_user = list(map(int, input("입력하고 싶은 단과 몇 번째까지 구할지?: ").split(","))) 
-dan = input_user[0]
-number = input_user[1]
-for i in range(2, dan+1):
-    array  = []
-    for j in range(1, number+1):
-        array.append(i * j)
+x, y = map(int,input("입력하고 싶은 단과 행?: ").split(","))
+for i in range(2, x+1):
+    array = []
+    for j in range(y):
+        array.append(i * (j+1)) 
     print(array)
+
+
